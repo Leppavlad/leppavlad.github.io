1c1d1244c8107b36394f2218a4289e98d5e49925
Vlad Leppa <leppavlad@gmail.com>
2020-08-07 17:15:25 +0400

    Добавлен массив мест

--- js/test.js
+++ js/test.js
@@ -1,12 +1,6 @@
-var map = L.map('mapid').setView([34.76, 113.65], 13);
-const sidebar = document.getElementById('sidebar');
-const sidebarClose = document.getElementById('sidebarClose');
-const title = document.getElementById('place-title');
-const coordinates = document.getElementById('coordinates');
-const description = document.getElementsByClassName('description-text');
-// const marker;
-
 // Инициализация карты
+var map = L.map('mapid').setView([34.76, 113.65], 13);
+
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
@@ -17,45 +11,78 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(map);
 // Делает карту кликабельной.
 map.on('click', onMapClick);
+/*========================================== */
 
 // Сайдбар
-  title.innerHTML = 'Место';
-  coordinates.innerHTML = 'Координаты x, y';
-  description[0].innerHTML = 'Выберите место на карте, чтобы узнать его описание';
-  description[1].innerHTML = '<img src="https://picsum.photos/600/200">';
+const sidebar = document.getElementById('sidebar');
+const sidebarClose = document.getElementById('sidebarClose');
+const title = document.getElementById('place-title');
+const coordinates = document.getElementById('coordinates');
+const description = document.getElementsByClassName('description-text');
 
-// Точка на карте
-var circle = L.circle([34.765, 113.655], {
+title.innerHTML = 'Место';
+coordinates.innerHTML = 'Координаты x, y';
+description[0].innerHTML = 'Выберите место на карте, чтобы узнать его описание';
+// description[1].innerHTML = '<img src="https://picsum.photos/600/200">';
+
+// Функции сайдбара
+function toggleSidebar() {
+  sidebar.classList.toggle('opened');
+}
+function openSidebar() {
+  if (!sidebar.classList.contains('opened'))
+    sidebar.classList.add('opened');
+}
+function closeSidebar() {
+  if (sidebar.classList.contains('opened'))
+    sidebar.classList.remove('opened');
+}
+/*========================================== */
+
+// Слой  маркера
+let marker = null;
+let markerPos = null;
+const markerGroup = L.layerGroup().addTo(map);
+
+// Слой заведений
+let places = [];
+const placesGroup = L.layerGroup(places).addTo(map);
+/*========================================== */
+
+let circleOptions = {
   color: 'red',
   fillColor: '#f03',
   fillOpacity: 0.5,
-  radius: 100
-}).addTo(map);
+  radius: 5,
+  title: 'My Location',
+  clickable: true
+};
 
-// Окно при нажатии точки на карте
-circle.bindPopup("I am a circle.");
-circle.on('click', function () { popSidebar(); })
+let markerOptions = {
+  title: ''
+};
+/*========================================== */
 
-// Группа для маркера
-var markerGroup = L.layerGroup().addTo(map);
-var marker = null;
-var markerPos = null;
 // Клик на карте создаёт маркер
 function onMapClick(e) {
   markerGroup.clearLayers();
   markerPos = e.latlng;
-  marker = L.marker(markerPos).addTo(markerGroup);
+  marker = L.marker(markerPos, markerOptions).addTo(markerGroup);
 }
 
-// Группа для мест
-const placesGroup = L.layerGroup().addTo(map)
 // Создаёт место на основе маркера
 function newPlace() {
-  var place = L.circle(markerPos, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 100
-  }).addTo(placesGroup);
+  var place = L.circleMarker(markerPos, circleOptions).addTo(placesGroup);
+  places.push({'id': places.length, 'lat': markerPos.lat, 'lng': markerPos.lng});
+  console.log(places);
+  // Убираем маркер
   markerGroup.clearLayers();
 }
+/*========================================== */
+
+// Существующая точка на карте
+var circle = L.circleMarker([34.765, 113.655], circleOptions).addTo(placesGroup);
+// console.log(places)
+// Окно при нажатии точки на карте
+circle.bindPopup("I am a circle.");
+// circle.on('click', alert('Gosh'));
