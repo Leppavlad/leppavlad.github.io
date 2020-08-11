08ee4dec9f1dbdce3954e496d1db5af20d3d5f71
Vlad Leppa <leppavlad@gmail.com>
2020-08-11 18:00:57 +0400

    галерея

--- js/base.js
+++ js/base.js
@@ -1,3 +1,18 @@
+// Галерея
+thumbs.onclick = function (event) {
+  let thumbnail = event.target.closest('a');
+
+  if (!thumbnail) return;
+  showThumbnail(thumbnail.href, thumbnail.title);
+  event.preventDefault();
+}
+
+function showThumbnail(href, title) {
+  largeImg.src = href;
+  largeImg.alt = title;
+}
+/*========================================== */
+
 // Инициализация карты
 var map = L.map('mapid').setView([34.76, 113.65], 13);
 
